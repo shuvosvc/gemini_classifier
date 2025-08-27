ec70ede7fac15a3c2b3067c8c6f3d6a6369b3230
shuvosvc <5arafatshovo@gmail.com>
2025-08-28 05:32:27 +0600

    chore: remove auth

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ const { userInfo } = require('os');
 app.use(cors());
 
 
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-// app.use('/profiles', express.static(path.join(__dirname, 'profiles')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/profiles', express.static(path.join(__dirname, 'profiles')));
 
-app.use('/uploads', authfilereq, express.static(path.join(__dirname, 'uploads')));
-app.use('/profiles', authfilereq, express.static(path.join(__dirname, 'profiles')));
+// app.use('/uploads', authfilereq, express.static(path.join(__dirname, 'uploads')));
+// app.use('/profiles', authfilereq, express.static(path.join(__dirname, 'profiles')));
 
 
 app.get("/", (_, res) => {
